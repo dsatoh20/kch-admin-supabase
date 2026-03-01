d39f7cfdf5d8129f77fa652b2bb1b0af8bb41254
dsatoh20 <dsatoh20@gmail.com>
2026-03-02 01:58:41 +0900

    updated meta description

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,8 +9,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata: Metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "金沢サークルハブ管理者サイト",
+  description: "金沢サークルハブの管理者向けサイトです。",
 };
 
 const geistSans = Geist({
